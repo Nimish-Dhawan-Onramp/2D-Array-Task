0e01dda74022691e03ed3a8a4e2832af1ee6d554
Nimish Dhawan <nimish@onramp.money>
2024-08-27 16:34:43 +0530

    4th commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
-
+import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
 const config = {
   kit: {
+	appDir: 'app',
     adapter: adapter({
       // Options can be configured here if needed
       fallback: 'index.html', // Used for SPA routing if applicable
     }),
     paths: {
-      base: '/2D-Array-Task' // Use a leading '/' but no trailing '/'
+      base: dev ? '' : process.env.BASE_PATH, // Use a leading '/' but no trailing '/'
     }
   },
-  preprocess: sveltePreprocess()
+  preprocess: vitePreprocess()
 };
 
 export default config;
